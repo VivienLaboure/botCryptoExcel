e71d40291031dd9b829b0af41f80d1d162a900f9
VivienLaboure <94540521+VivienLaboure@users.noreply.github.com>
2023-04-14 22:45:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,49 @@
-
 const config = require('./config.json');
 const Excel = require('exceljs');
-const { Client, Intents } = require('discord.js');
+const { Client } = require('discord.js');
 
-
-const bot = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
-});
+const bot = new Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
 
 bot.on('ready', () => {
-
     console.log(`${bot.user.tag} is online...\n \n`);
-    bot.user.setActivity('Vouais', { type: 'PLAYING' });
+    bot.user.setActivity('Écoute', { type: 'PLAYING' });
 });
 
+bot.on('messageCreate', async (interaction) => {
+    const coinRegex = /^(SOL|BTC|ETH)\s+(\d+)$/;
+    const match = interaction.content.match(coinRegex);
+    if (match) {
+        const [, coin, amount] = match;
 
-bot.on('message', interaction => {
-
-    if (interaction.content.startsWith("SOL") || interaction.content.startsWith("BTC") || interaction.content.startsWith("ETH")) {
-
-        let coin = interaction.content.split(" ")[0];
-        let amount = interaction.content.split(" ")[1];
-
-        let date = new Date();
-
-	let month = date.getMonth() + 1;
-
-        let datePost = date.getUTCDate() + "/" + month + "/" + date.getUTCFullYear();
-
-
-        let hours = date.getHours() + "h" + date.getMinutes();
-
-
-        let filename1 = 'crypto.xlsx';
-
-        let workbook = new Excel.Workbook();
+        const date = new Date();
+        const month = date.getMonth() + 1;
+        const datePost = `${date.getUTCDate()}/${month}/${date.getUTCFullYear()}`;
+        const hours = `${date.getHours()}h${date.getMinutes()}`;
 
         let bgColor;
-
-        if (coin == "SOL") {
-            bgColor = '#cf03cf';
-
-        } if (coin == "BTC") {
-            bgColor = '#ffee55';
-
-        } if (coin == "ETH") {
-            bgColor = '#0096ff';
-
-        } else {
-            bgColor = '#ffffff';
+        switch (coin) {
+            case 'SOL':
+                bgColor = '#cf03cf';
+                break;
+            case 'BTC':
+                bgColor = '#ffee55';
+                break;
+            case 'ETH':
+                bgColor = '#0096ff';
+                break;
+            default:
+                bgColor = '#ffffff';
         }
 
-        workbook.xlsx.readFile(filename1)
-            .then(() => {
-
-                workbook.getWorksheet('Feuille 1').addRow([coin, amount, datePost, hours]);
-
-                return workbook.xlsx.writeFile(filename1);
-            }).then(() => {
-                console.log('File is written');
-            }).catch(err => console.error(err));
+        const filename1 = 'crypto.xlsx';
+        const workbook = new Excel.Workbook();
+        await workbook.xlsx.readFile(filename1);
+        workbook.getWorksheet('Feuille1').addRow([coin, amount, datePost, hours]);
+        workbook.xlsx.writeFile(filename1);
+        console.log('File is written');
     }
 });
 
-
 bot.login(config.TOKEN).catch((err) => {
-    bot.logger.error(err);
+    console.error(err);
 });
